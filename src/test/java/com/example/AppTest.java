c6f7605f8ffefdfa39953a3cbfb825ce3d519bac
刘玉雷 <qpzm056511>
2024-07-19 20:35:58 +0800

    笔试题1

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -2,7 +2,10 @@ package com.example;
 
 public class AppTest {
     public static void main(String[] args) {
-
+        Father f = new Son();
+        //方法看右边
+        //属性看左边 属性没有多态
+        System.out.println(f.x);
     }
 
 }
---
+++ src/test/java/com/example/Son.java
@@ -0,0 +1,14 @@
+package com.example;
+
+class Son extends Father {
+    int x = 30;
+
+    public Son() {
+        this.print();
+        x = 40;
+    }
+
+    public void print() {
+        System.out.println("Son.x= " + x);
+    }
+}
